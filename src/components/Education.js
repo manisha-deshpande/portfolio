30e79bec415e349a107a86b7f148014992141948
manisha-deshpande <dmanisha.work@gmail.com>
2024-05-07 13:54:42 -0700

    added an education section

---
+++ src/components/Education.js
@@ -0,0 +1,118 @@
+import React, { Component } from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import Badge from "react-bootstrap/Badge";
+import ExperienceDetailsModal from "./ExperienceDetailsModal";
+
+class Education extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deps: {},
+      workDetailsModalShow: false,
+    };
+  }
+
+  render() {
+    let workDetailsModalShow = (data) => {
+      this.setState({ workDetailsModalShow: true, deps: data });
+    };
+
+    let workDetailsModalClose = () => this.setState({ workDetailsModalShow: false });
+    if (this.props.educationDegrees && this.props.resumeBasicInfo) {
+      var sectionName = this.props.resumeBasicInfo.section_name.education;
+      var works = this.props.educationDegrees.map(function (work, i) {
+        const technologies = work.technologies;
+        const mainTechnologies = work.mainTech;
+
+        var mainTech = mainTechnologies.map((technology, i) => {
+          return (
+            <Badge pill className="main-badge mr-2 mb-2" key={i}>
+              {technology}
+            </Badge>
+          );
+        });
+        var tech = technologies.map((technology, i) => {
+          return (
+            <Badge pill className="experience-badge mr-2 mb-2" key={i}>
+              {technology}
+            </Badge>
+          );
+        });
+
+        return (
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date={work.years}
+            iconStyle={{
+              background: "#AE944F",
+              color: "#fff",
+              textAlign: "center",
+            }}
+            icon={<img src={work.logo_url} alt="Company Logo" style={{ width: "100%", height: "100%" }} />}
+            key={i}
+          >
+            <div style={{ textAlign: "left", marginBottom: "4px" }}>
+              {mainTech}
+            </div>
+
+            <h3
+              className="vertical-timeline-element-title"
+              style={{ textAlign: "left" , cursor: "pointer"}}
+
+              onClick={() => workDetailsModalShow(work)}
+            >
+              {work.title}
+            </h3>
+            <h4
+              className="vertical-timeline-element-subtitle"
+              style={{ textAlign: "left" }}
+            >
+              {work.company}
+            </h4>
+            <div style={{ textAlign: "left", marginTop: "15px" }}>{tech}</div>
+          </VerticalTimelineElement>
+        );
+      });
+    }
+
+    return (
+      <section id="education" className="pb-5">
+        <div className="col-md-12 mx-auto">
+          <div className="col-md-12">
+            <h1 className="section-title" style={{ color: "black" }}>
+              <span className="text-black" style={{ textAlign: "center" }}>
+                {sectionName}
+              </span>
+            </h1>
+          </div>
+        </div>
+        <div className="col-md-8 mx-auto">
+          <VerticalTimeline>
+            {works}
+            <VerticalTimelineElement
+              iconStyle={{
+                background: "#AE944F",
+                color: "#fff",
+                textAlign: "center",
+              }}
+              icon={
+                <i className="fas fa-hourglass-start mx-auto experience-icon"></i>
+              }
+            />
+          </VerticalTimeline>
+          <ExperienceDetailsModal
+            show={this.state.workDetailsModalShow}
+            onHide={workDetailsModalClose}
+            data={this.state.deps}
+          />
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Education;
